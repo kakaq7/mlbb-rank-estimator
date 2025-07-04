e1cd85b8b83859d4ba4ec771f5942628f296479d
kakaq7 <93967645+kakaq7@users.noreply.github.com>
2025-07-05 06:51:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ else:
 
 # Footer
 st.markdown("---")
-st.markdown("**Dibuat oleh [@al.ismaill](https://instagram.com/al.ismaill)**"
+st.markdown("**Dibuat oleh [@al.ismaill](https://instagram.com/al.ismaill)**")
